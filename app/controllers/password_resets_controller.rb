f273350d93d922824b7268dd8aa7719bcc372305
adek06 <adek06@163.com>
2019-10-04 14:12:17 +0800

    add reset col and finished form of reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,19 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: param[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "重設鏈接已經發送到郵箱裏，請儘快查收"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "該郵箱未注冊"
+      render 'new'
+    end
+  end
+
   def edit
   end
 end
