0107261b856652e6e55ecb8474fa57e9a094336a
adek06 <adek06@163.com>
2019-10-04 15:23:36 +0800

    reset form and contorller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,9 +1,12 @@
-class PasswordResetsController < ApplicationController
+aclass PasswordResetsController < ApplicationController
+  before_action :get_user,   only:[:edit, :update]
+  before_action :valid_user, only:[:edit, :update]
+
   def new
   end
 
   def create
-    @user = User.find_by(email: param[:password_reset][:email].downcase)
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
@@ -17,4 +20,17 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id])
+              )
+          redirect_to root_url
+      end
+    end
 end
